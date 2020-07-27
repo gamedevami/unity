b31fe2278626607742dc449c0cbdad3caf85faa2
Kozova1 <mug66kk@gmail.com>
2020-07-28 01:11:32 +0300

    TODO: make it so that it detects when a piece falls and then does the Game Over stuff

---
+++ New Unity Project (3)/Assets/Scripts/CreateStackTile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStackTile : MonoBehaviour {
+    private Vector3 mousePos;
+    private Vector3 objectPos;
+    public GameObject yourPrefab;
+    private int height = 0;
+
+
+    int GetStackHeight() {
+        GameObject highest = gameObject;
+        GameObject[] foundStackTiles = GameObject.FindGameObjectsWithTag("StackTile");
+        foreach(GameObject curr in foundStackTiles) {
+            if(curr.transform.position.y > highest.transform.position.y) {
+                highest = curr;
+            }
+        }
+        return (int)highest.transform.position.y / 2;
+    }
+
+    void Update() {
+        int lastHeight = height;
+        Debug.Log(GetStackHeight());
+        height = Mathf.Max(GetStackHeight(), height);
+        if (lastHeight < height) { GameOver();  }
+        if(Input.GetButtonDown("Fire1")) {
+            mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;
+            Camera.main.transform.position = new Vector3(
+                Camera.main.transform.position.x,
+                Camera.main.transform.position.y + 2.0f,
+                Camera.main.transform.position.z
+            );
+            objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(yourPrefab, objectPos, Quaternion.identity);
+        }
+    }
+    void GameOver() { Debug.Log("You lose!");  } //TODO: Implement this
+}
---
+++ New Unity Project (3)/Assets/Scripts/WreckingBallController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class WreckingBallController : MonoBehaviour {
+    public float TotalSpeed;
+    private Vector3 DirectionalSpeed;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        mousePos.x = Input.mousePosition.x;
+        mousePos.y = Input.mousePosition.y;
+        mousePos.z = 10f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.y = 2f;
+        transform.position = Vector3.Lerp(transform.position, mousePos, Time.deltaTime);
+    }
+}
