3e76eb4796e7c1bddd062fb5358b4c2c557dcc51
Kozova1 <mug66kk@gmail.com>
2020-08-01 19:00:05 +0300

    maybe new mechanic?

--- bigboibracky/Assets/moveme.cs
+++ bigboibracky/Assets/moveme.cs
@@ -1,35 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class moveme : MonoBehaviour
-{
+public class moveme : MonoBehaviour {
 
     public Vector3 jump;
     public float jumpForce = 2.0f;
-
-    public bool isGrounded;
+    public float speed = 5f;
+    public float xSpeed = 0f;
+    private bool isJumping = false;
+    private bool isGrounded = false;
+    private bool isBtnPressed = false;
     Rigidbody rb;
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        jump = new Vector3(0f, 1f, 0f);
     }
 
-    void OnCollisionStay()
-    {
+    void OnCollisionStay() {
         isGrounded = true;
     }
-    void OnCollisionExit()
-    {
+    void OnCollisionExit() {
         isGrounded = false;
     }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
 
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+    void Update() {
+        isBtnPressed = false;
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            isJumping = true;
             isGrounded = false;
+        }
+        bool isAPressed = Input.GetKey(KeyCode.A);
+        bool isDPressed = Input.GetKey(KeyCode.D);
+        if(isAPressed ^ isDPressed) {
+            xSpeed = isAPressed ? speed : -speed;
+        } else {
+            xSpeed = 0;
+        }
+    }
+
+    void FixedUpdate() {
+        Vector3 velocity = rb.velocity;
+        if(isJumping) {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isJumping = false;
+        }
+        if(xSpeed != 0) {
+            rb.AddForce(new Vector3(xSpeed, 0, 0) * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            velocity.x = Mathf.Clamp(velocity.x, -speed, speed);
+            rb.velocity = velocity;
         }
     }
 }
