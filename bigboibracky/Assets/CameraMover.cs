bf1fe7127fee35bdb3b2f0dfa5ca179a0f5c8dab
Kozova1 <mug66kk@gmail.com>
2020-08-01 19:21:30 +0300

    Added weird movement style

---
+++ bigboibracky/Assets/CameraMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+    [SerializeField] private Transform playerPos;
+    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private float freezedYCoordinate;
+    // Update is called once per frame
+    void Update() {
+        Vector3 pos = playerPos.position;
+        pos.y = freezedYCoordinate;
+        transform.position = pos + cameraOffset;
+    }
+}
--- bigboibracky/Assets/moveme.cs
+++ bigboibracky/Assets/moveme.cs
@@ -3,14 +3,12 @@ using System.Collections;
 
 public class moveme : MonoBehaviour {
 
-    public Vector3 jump;
-    public float jumpForce = 2.0f;
-    public float speed = 5f;
-    public float xSpeed = 0f;
+    private Vector3 jump;
+    [SerializeField] private float jumpForce = 2.0f;
+    [SerializeField] private float speed = 0.5f;
     private bool isJumping = false;
     private bool isGrounded = false;
-    private bool isBtnPressed = false;
-    Rigidbody rb;
+    private Rigidbody rb;
     void Start() {
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0f, 1f, 0f);
@@ -24,30 +22,19 @@ public class moveme : MonoBehaviour {
     }
 
     void Update() {
-        isBtnPressed = false;
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+        bool isAPressed = Input.GetKeyDown(KeyCode.A);
+        bool isDPressed = Input.GetKeyDown(KeyCode.D);
+        if(isGrounded && (isAPressed ^ isDPressed)) {
             isJumping = true;
             isGrounded = false;
-        }
-        bool isAPressed = Input.GetKey(KeyCode.A);
-        bool isDPressed = Input.GetKey(KeyCode.D);
-        if(isAPressed ^ isDPressed) {
-            xSpeed = isAPressed ? speed : -speed;
-        } else {
-            xSpeed = 0;
+            jump.x = isAPressed ? speed : -speed;
         }
     }
 
     void FixedUpdate() {
-        Vector3 velocity = rb.velocity;
         if(isJumping) {
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isJumping = false;
-        }
-        if(xSpeed != 0) {
-            rb.AddForce(new Vector3(xSpeed, 0, 0) * Time.fixedDeltaTime, ForceMode.VelocityChange);
-            velocity.x = Mathf.Clamp(velocity.x, -speed, speed);
-            rb.velocity = velocity;
         }
     }
 }
