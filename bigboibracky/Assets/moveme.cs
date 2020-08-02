679f52d813fc04e97d183d1128b74e155fb1b4b4
Kozova1 <mug66kk@gmail.com>
2020-08-02 16:15:36 +0300

    did some stuff

--- bigboibracky/Assets/moveme.cs
+++ bigboibracky/Assets/moveme.cs
@@ -9,16 +9,20 @@ public class moveme : MonoBehaviour {
     private bool isJumping = false;
     private bool isGrounded = false;
     private Rigidbody rb;
+    private Animator anim;
     void Start() {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
         jump = new Vector3(-0.4f, 0.2f, 0f);
     }
 
     void OnTriggerStay() {
         isGrounded = true;
+        anim.ResetTrigger("Jump");
     }
     void OnTriggerExit() {
         isGrounded = false;
+        anim.SetTrigger("Jump");
     }
 
     void Update() {
@@ -28,6 +32,7 @@ public class moveme : MonoBehaviour {
             isJumping = true;
             isGrounded = false;
             jump.x = isAPressed ? speed : -speed;
+            transform.Rotate(new Vector3(0, 0, 0));
         }
     }
 
