7c4809d38d94b377550fb64a744f5619bf895556
kozova1 <mug66kk@gmail.com>
2020-08-04 12:00:39 +0300

    Changed Jump Pad ForceMode to ForceMode.Impulse from implicit ForceMode.Force

--- bigboibracky/Assets/NewBehaviourScript.cs
+++ bigboibracky/Assets/NewBehaviourScript.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
-     {
-         if (other.gameObject.CompareTag("Player"))
-         {
-             other.gameObject.GetComponent<Rigidbody>
-                     ().AddForce(Vector3.up * 3f);
-         }
-     }
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject
+                .GetComponent<Rigidbody>()
+                .AddForce(Vector3.up * 3f, ForceMode.Impulse);
+        }
+    }
 }
 
